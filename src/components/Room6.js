1a701508e3895be0963927c0a0462af219ed797d
Sadia Saleem <64151837+SadiaSaleem12@users.noreply.github.com>
2022-01-18 10:15:02 -0800

    Update Room6.js

--- src/components/Room6.js
+++ src/components/Room6.js
@@ -3,19 +3,9 @@ import React, { Component, useState } from "react";
 import { Form, Button, Nav, Navbar, Container } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 import { FaPencilAlt } from "react-icons/fa";
+import {Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  getFirestore,
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  deleteField,
-  updateDoc
-} from "firebase/firestore/lite";
-import { db } from "../firebase/index";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
 
 const Room6 = (props) => {
   const [values, setValues] = useState({
@@ -30,47 +20,13 @@ const Room6 = (props) => {
     console.log(values);
   };
   const showForm = () => {
-      const saveChanges = async () => {
-        const firebaseConfig = {
-          apiKey: "AIzaSyDJvLhy3fSxVTHSOyT32X70UPTWK2UQHfM",
-          authDomain: "firestore-app-3af45.firebaseapp.com",
-          projectId: "firestore-app-3af45",
-          storageBucket: "firestore-app-3af45.appspot.com",
-          messagingSenderId: "617491569351",
-          appId: "1:617491569351:web:5efa7bf5010a5c00fe7ce2",
-          measurementId: "G-235WTYHX3D",
-        };
-        const app = initializeApp(firebaseConfig);
-         const auth = getAuth(app);
-         const db = getFirestore(app);
-      if (id = "r1-tues-slot1")
-       { await addDoc(collection(db, "room1/tuesday/slot1"), {
-          course: values.course,
-          date: values.date,
-        }).then((res) => {
-            console.log("Slot entered successfully");
-            alert("Slot reserved successfully")
-                         
-            var div = document.getElementById("r1-tues-slot1");
-            var p = document.createElement("p");
-            p.textContent = values.course;
-            console.log(p);
-            div.appendChild(p);
-            console.log("Slot entered successfully");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-       }
-              
-    };
+  
     return (
 
       <div>
-        <Form >
+       <Form id="formclass">
           <Form.Group className="mt-3">
-            <Form.Label>Enter Class Details</Form.Label>
+            <Form.Label id="label">Add Class Details</Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter course name"
@@ -85,11 +41,12 @@ const Room6 = (props) => {
               id="date"
               onChange={handleChange}
             />
-            <Button
+            <Button id="submitbtn"
               onClick={() => {
                 setShowForm(false);
-                saveChanges();
-              }}>
+                //saveChanges();
+              }}
+            >
               Submit
             </Button>
           </Form.Group>
@@ -100,21 +57,23 @@ const Room6 = (props) => {
 
   return (
     <div>
+      <Link to="/Dashboard"> <button id="dashbtn">Dashboard</button></Link>
+<h1 id="mainhead">Departmental Resource Management System</h1>
+          {showFormval ? showForm() : null}
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Classrooms</Navbar.Brand>
+        <Navbar.Brand id="navbarbrand">Classrooms</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/classroom">Room1</Nav.Link>
-              <Nav.Link href="/room2">Room2</Nav.Link>
-              <Nav.Link href="/room3">Room3</Nav.Link>
-              <Nav.Link href="/room4">Room4</Nav.Link>
-              <Nav.Link href="/room5">Room5</Nav.Link>
-              <Nav.Link href="/room6">Room6</Nav.Link>
-              <Nav.Link href="/classroom">Room7</Nav.Link>
-              <Nav.Link href="/room8">Room8</Nav.Link>
-
+              <Nav.Link href="/classroom" id="navbarlinks">Room1</Nav.Link>
+              <Nav.Link href="/room2" id="navbarlinks">Room2</Nav.Link>
+              <Nav.Link href="/room3" id="navbarlinks">Room3</Nav.Link>
+              <Nav.Link href="/room4" id="navbarlinks">Room4</Nav.Link>
+              <Nav.Link href="/room5" id="navbarlinks">Room5</Nav.Link>
+              <Nav.Link href="/room6" id="navbarlinks">Room6</Nav.Link>
+              <Nav.Link href="/classroom" id="navbarlinks">Room7</Nav.Link>
+              <Nav.Link href="/room8" id="navbarlinks">Room8</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -136,8 +95,8 @@ const Room6 = (props) => {
             <td id="slot1">8:30-9:20</td>
             <td>
                 <div className='row' id="r1-mon-slot1">
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true); setId("r1-thurs-slot1")}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
              <td>
@@ -155,8 +114,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
@@ -179,8 +138,8 @@ const Room6 = (props) => {
                 <td>
                 <div className='row'>
                
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                 </td>
@@ -192,8 +151,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
@@ -214,8 +173,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
@@ -226,8 +185,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                 </td>
@@ -250,8 +209,8 @@ const Room6 = (props) => {
                         </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
@@ -263,8 +222,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
@@ -286,8 +245,8 @@ const Room6 = (props) => {
                         </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>     
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>     
                 </div>
                 </td>
                 <td>
@@ -299,8 +258,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>  
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div> 
                   
                 </div>
                 </td>
@@ -323,9 +282,8 @@ const Room6 = (props) => {
                         </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
-         
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                 </td>
@@ -338,15 +296,15 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
              </tr> 
@@ -361,8 +319,8 @@ const Room6 = (props) => {
                         </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
          
                   
@@ -386,8 +344,8 @@ const Room6 = (props) => {
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
              </tr> 
@@ -395,36 +353,36 @@ const Room6 = (props) => {
                 <td id="slot6">3:40-4:30</td>
                 <td>
                   <div className='row'>
-      <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'><FaTrash /></Button></div>
+                  <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                         </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
          
                   
                 </div>
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                   
                 </div>
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
                 <td>
                 <div className='row'>
-                <div className='col-md-4'><Button className='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
-                  <div className='col-md-4'><Button className='btns'> <FaTrash /></Button></div>
+                <div className='col-md-4'><Button id='btns' onClick={() => {setShowForm(true)}}><FaPencilAlt/></Button></div>
+                  <div className='col-md-4'><Button id='btns'><FaTrash /></Button></div>
                 </div>
                 </td>
              </tr> 
